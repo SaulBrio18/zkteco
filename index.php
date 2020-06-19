67df5ebecb13c134b2291a92ef9bb74b21ce2aef
Saul Briones <saul@fatrian.com>
2020-06-18 20:37:43 -0500

    index :v

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php?>
+
+
+<form action="form_usuario.php" method="POST">
+    <input type="submit" value="nuevo usuario"/>
+</form>
+
+<form action="form_buscaUser.php" method="POST">
+    <input type="submit" value="buscar usuario"/>
+</form>
+
+<form action="asistencia.php" method="POST">
+    <input type="submit" value="asistencia"/>
+</form>
